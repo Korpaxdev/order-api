07b8ed37637e20055fc9f09157c29babd8ebf653
korpaxdev <korpaxdev@gmail.com>
2023-08-13 17:27:25 +0300

    add jwt login

--- backend/urls.py
+++ backend/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 
 from backend.views.shop_views import ShopDetailView, ShopListView, ShopPriceListView, ShopUpdateStatusView
 from backend.views.user_views import UserProfileView, UserRegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     # user
     path("users/profile/", UserProfileView.as_view(), name="profile"),
     path("users/register/", UserRegisterView.as_view(), name="register"),
+    path("users/token/", TokenObtainPairView.as_view(), name="token"),
+    path("users/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     # shop
     path("shops/", ShopListView.as_view(), name="shops"),
     path("shops/<slug>/", ShopDetailView.as_view(), name="shop_detail"),
